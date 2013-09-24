d3be515debfb8154aba6882227eb0cac343ac6b8
sananth12 <sananthanatarajan12@gmail.com>
2013-09-25 01:13:06 +0530

    virtual contest

---
+++ codeforces/334a.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+   int t,count=0;
+   char s[2],a[2];
+   scanf("%d",&t);
+   scanf("%s",a);
+   s[0]=a[0];
+   s[1]=a[1];
+   t--;
+   while(t--)
+   {
+        scanf("%s",a);
+        if(a[0]!=s[0])
+        {
+            s[0]=a[0];
+            count++;
+        }
+   }
+   printf("%d\n",count);
+return 0;
+}
---
+++ codeforces/334a.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string>
+#include<iostream>
+using namespace std;
+int main()
+{
+   int t,count=1,flag=0;
+   string a,s;
+   scanf("%d",&t);
+   while(t--)
+   {
+        cin>>a;
+        if(flag==0)
+        {
+            flag++;
+            s=a;
+        }
+        if(a!=s)
+        {
+            s=a;
+            count++;
+        }
+      //  printf("**%d**\n",t);
+   }
+   printf("%d",count);
+return 0;
+}
