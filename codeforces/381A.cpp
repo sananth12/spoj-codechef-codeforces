8a616c5d3114089e703e49e7c0f42108c36fb899
sananth12 <sananthanatarajan12@gmail.com>
2014-01-13 00:04:00 +0530

    Codeforces 381

---
+++ codeforces/381A.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+   int n;
+   scanf("%d",&n);
+   int i,arr[n];
+
+   for(i=0;i<n;i++)
+    cin>>arr[i];
+
+   int s=0,d=0,l=0,r=n-1;
+   while(l<=r)
+   {
+       if(arr[l]>arr[r])
+        {
+            s+=arr[l];
+            l++;
+        }
+       else
+       {
+           s+=arr[r];
+           r--;
+       }
+
+       if(l>r)
+            break;
+
+       if(arr[l]>arr[r])
+        {
+            d+=arr[l];
+            l++;
+        }
+       else
+       {
+           d+=arr[r];
+           r--;
+       }
+   }
+
+   cout<<s<<" "<<d;
+
+return 0;
+}
---
+++ codeforces/381B.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n,i;
+    cin>>n;
+
+    int a[n];
+    for(i=0;i<n;i++)
+        cin>>a[i];
+
+    sort(a,a+n);
+
+    int map[100000]={0},uni=0,max=a[n-1],cnt=0;
+    vector < int > ans,r;
+    vector<int>::iterator it;
+
+    for(i=0;i<n;i++)
+    {
+        map[a[i]]++;
+        it = r.begin();
+
+        if(map[a[i]]==1)
+              ans.push_back(a[i]);
+
+
+         if(map[a[i]]==2 && a[i]!=max)
+             r.insert(it,a[i]);
+
+    }
+    cout<<ans.size()+r.size()<<endl;
+
+    for(it=ans.begin();it!=ans.end();++it)
+        cout<<*it<<" ";
+    for(it=r.begin();it!=r.end();++it)
+        cout<<*it<<" ";
+
+return 0;
+}
